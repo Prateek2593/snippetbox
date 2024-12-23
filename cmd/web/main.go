81b208a51d413a8acff5e9cbedf8cda07d274072
Prateek Patidar <patidarprateek2593@gmail.com>
2024-12-23 14:00:18 +0530

    serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,13 @@ func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home handler function at the root ("/") path.
 	mux := http.NewServeMux()
+
+	// create a file server which serves files out of the "./ui/static" directory. note that the path given to the http.Dir function is relative to the project root
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// use the mux.Handle() function to register the file server as the handler for all url paths that start with "/static/". for matching paths, we strip the "/static" prefix before the request reaches the file server
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
