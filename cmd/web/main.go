922e456147581879bbefdbc06b92cb8c60ce034e
Prateek Patidar <patidarprateek2593@gmail.com>
2024-12-24 16:46:32 +0530

    mysql setup and mysql driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // define an application struct to hold the application wide dependencies for the web application. for now we'll only include fields for the two custom loggers, but we'll add more to it as the build progress
@@ -18,6 +21,9 @@ func main() {
 	// define a new command line flag with name addr, a default value of ":4000" and some short help text explaining what the flag controls. the value of the flag will be stored in the addr variable at runtime.
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
+	// define a new command line flag for the MySQL DSN string
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+
 	// importantly, we use the flag.Parse() function to parse the command line flag. this reads in command line flag value and assigns it to the addr variable. you need to call this *before* you use the addr variable otherwise it will always contain the default value of ":4000". if any errors are encountered during parsing the application will be terminated
 	flag.Parse()
 
@@ -26,6 +32,15 @@ func main() {
 
 	// create a logger for writing error messages in the same way, but use stderr as the destination and use the log.Lshortfile flag to include the relevant file name and line number
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// to keep the main() function tidy we have put the code for creating a connection pool into separate openDB() function below. we pass openDB() tht dsn from command line flag
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// we also defer a call to db.Close(), so that the connection pool is closed before main function exits
+	defer db.Close()
 
 	// create a new instance of our application struct with the custom loggers
 	app := &application{
@@ -47,6 +62,18 @@ func main() {
 	// err := http.ListenAndServe(*addr, mux)
 
 	// call the ListenAndServe() method on our new http.Server struct
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// the openDB() function wraps sql.Open() and returns a sql.DB connection pool for a given DSN
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
