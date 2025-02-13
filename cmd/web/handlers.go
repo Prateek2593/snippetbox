833430b6a6ed930f620f47399dd7932d7be8ea58
Prateek Patidar <patidarprateek2593@gmail.com>
2025-02-14 00:27:06 +0530

    storing the user details and hashing password

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -330,7 +330,23 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintln(w, "create a new user")
+	// try to create a new user record in the database. if the email already exists tehn add an error message to the form and re display it
+	err = app.users.Insert(form.Name, form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.AddFieldError("email", "Email address is already in use")
+
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "signup.tmpl", data)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	app.sessionManager.Put(r.Context(), "flash", "Your signup was successful. Please login")
+
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
